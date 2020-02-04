a09ea423c5903227f1bea93bf22e331c6d3ad99c
Will Johnston <wijohnst@gmail.com>
2020-02-04 18:45:04 -0500

    serverside debug

--- src/components/headerBoxes.js
+++ src/components/headerBoxes.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import "./headerBoxes.css";
 import HeaderBox from "./headerBox";
 import PrintLogo from "./printLogo";
@@ -7,29 +7,36 @@ import PrintLogo from "./printLogo";
 
 const HeaderBoxes = ({handleView}) => {
   
+  const [boxes, setBoxes] = useState(0);
+
+  useEffect( () =>{
   const uuid = require('uuid/v1');
 
-  const displayDimensions = () =>{
+  // const displayDimensions = () =>{
     let width = Math.floor(window.innerWidth/150);
     let boxes = [];
     let colorVal = 161;
     
-    for(let x = 1; x <= (width - 1); x++){
+    for(let x = 1; x <= (width - 1
+      ); x++){
       let key = uuid();
       boxes.push(<HeaderBox key={key} color= {colorVal}  index={x} handleView={handleView}/>)
       colorVal = colorVal + (14 + x);
-    }
-    return boxes;
-  }
+    
+    setBoxes(boxes);
   
-  let boxes = displayDimensions();
-  window.addEventListener("resize", displayDimensions.bind());
+  }}, [handleView]) 
+  // let boxes = displayDimensions();
+  // window.addEventListener("resize", displayDimensions.bind());
+  
   return (
     <div id="boxes-wrapper">
       <PrintLogo />
       {boxes}
     </div>
   );
+
+  
 }
 
 export default HeaderBoxes;
