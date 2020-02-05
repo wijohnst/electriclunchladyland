9f6d6c4b0ebcd3ed0b60e40023fcd8f76709ef48
Will Johnston <wijohnst@gmail.com>
2020-02-05 08:59:42 -0500

    added logo & banner

---
+++ src/components/El3Banner.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './El3Banner.css'
+
+export default function El3Banner() {
+  return (
+    <div id="el3-banner-wrapper">
+      <div id="el3-banner-e2e-wrapper">
+        <span>end-to-end</span>
+      </div>
+      <div id="el3-banner-hospitality-wrapper">
+        <span>hospitality</span>
+      </div>
+      <div id="el3-banner-specialists-wrapper">
+        <span>specialists</span>
+      </div>
+    </div>
+  )
+}
---
+++ src/components/El3Logo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./El3Logo.css";
+export default function El3Logo(){
+  return (
+    <div className="card" id="el3-logo-wrapper">
+      
+      <div id="el3-logo-text-wrapper">
+        <div id="el3-logo-el-wrapper">EL</div>
+        <div id="el3-logo-3-wrapper">3</div>
+        <div id="el3-logo-consultants-wrapper">Consultants</div>
+      </div>
+    </div>
+  );
+}
+
+
